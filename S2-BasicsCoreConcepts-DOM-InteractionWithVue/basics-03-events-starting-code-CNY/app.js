d05ac41f05b22dd2e00b26463cac5e59ef203281
Vanessa <vantaidesign8@gmail.com>
2022-03-22 16:31:48 +0000

    CNY app to practice events using counter

--- S2-BasicsCoreConcepts-DOM-InteractionWithVue/basics-03-events-starting-code-CNY/app.js
+++ S2-BasicsCoreConcepts-DOM-InteractionWithVue/basics-03-events-starting-code-CNY/app.js
@@ -1,3 +1,13 @@
+/*
+Basic and Core Concepts - DOM Interaction with Vue.
+1. Events & Methods:
+It's best practice to write most of the logic in our Javascript code and example is updating the counter logic for this CNY app is in our Javascript code which is best practice.
+
+
+
+
+*/
+
 const app = Vue.createApp({
   data() {
     return {
